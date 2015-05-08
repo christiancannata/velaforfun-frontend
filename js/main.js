a648c8c616827e239c446fdfec02eee69c067ab7
christiancannata <christian.cannata@facile.it>
2015-05-08 11:57:47 +0200

    wip template

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+jQuery(document).ready(function ($) {
+
+    WebFontConfig = {
+        google: { families: [ 'Chivo:400,400italic,900:latin' ] }
+    };
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
+
+
+    
+    $(".delete-entity").click(function () {
+        var button=$(this);
+        $("button").attr("disabled", "disabled");
+        $.ajax({
+            type: "POST",
+            url: $(this).attr('data-route'),
+            success: function (response) {
+                if (response.success == true) {
+                    button.closest("tr").fadeOut();
+                    showalert($("#response-div"), "Eliminato con successo!", "success");
+
+                } else {
+                    showalert($("#response-div"), "Errore nell'eliminazione", "error");
+                }
+                $("button").removeAttr("disabled");
+
+            }
+        });
+    });
+
+});
