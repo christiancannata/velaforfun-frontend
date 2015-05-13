ccfedc1a734c3647bd961232513f40d4fe893bdd
christiancannata <christian.cannata@facile.it>
2015-05-13 12:55:12 +0200

    wip js maps

---
+++ js/porti.js
@@ -0,0 +1,85 @@
+/**
+ * Created by christiancannata on 13/05/15.
+ */
+$(document).ready(function () {
+
+
+
+    // Provide your access token
+    L.mapbox.accessToken = 'pk.eyJ1IjoiY2hyaXN0aWFuMTQ4OCIsImEiOiJZaldjZlM0In0.hXiRMyyCDLdQZUrqXF2eNw';
+    // Create a map in the div #map
+    var map = L.mapbox.map('map', 'examples.map-zr0njcqy');
+    var myLayer = L.mapbox.featureLayer().addTo(map);
+    var markerList = document.getElementById('marker-list');
+    map.on('click', function (e) {
+        $("#latitudine").val(e.latlng.lat);
+        $("#longitudine").val(e.latlng.lng);
+
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?lat="+e.latlng.lat+"&lon="+e.latlng.lng,
+            dataType: "json",
+            success: function(response){
+
+                console.log(response);
+            }
+        });
+
+    });
+
+
+    map.featureLayer.on('ready', function (e) {
+        map.featureLayer.eachLayer(function (layer) {
+            var item = markerList.appendChild(document.createElement('li'));
+            item.innerHTML = layer.toGeoJSON().properties.title;
+            item.onclick = function () {
+                map.setView(layer.getLatLng(), 14);
+                layer.openPopup();
+            };
+        });
+    });
+
+// This uses the HTML5 geolocation API, which is available on
+// most mobile browsers and modern browsers, but not in Internet Explorer
+//
+// See this chart of compatibility for details:
+// http://caniuse.com/#feat=geolocation
+    if (!navigator.geolocation) {
+        geolocate.innerHTML = 'Geolocation is not available';
+    } else {
+        geolocate.onclick = function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            map.locate();
+        };
+    }
+
+// Once we've got a position, zoom and center the map
+// on it, and add a single marker.
+    map.on('locationfound', function (e) {
+        map.fitBounds(e.bounds);
+
+        myLayer.setGeoJSON({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [e.latlng.lng, e.latlng.lat]
+            },
+            properties: {
+                'title': 'Here I am!',
+                'marker-color': '#ff8888',
+                'marker-symbol': 'star'
+            }
+        });
+
+        // And hide the geolocation button
+        geolocate.parentNode.removeChild(geolocate);
+    });
+
+// If the user chooses not to allow their location
+// to be shared, display an error message.
+    map.on('locationerror', function () {
+        geolocate.innerHTML = 'Position could not be found';
+    });
+
+
+});
